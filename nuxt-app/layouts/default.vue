c981533bfaaac325cc63066ecaa9a6b524c4bd96
sasanqua <78304794+sasanqua-dev@users.noreply.github.com>
2024-06-25 22:44:28 +0900

    chore: Add Nuxt app files and configurations

---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div class="hidden navbar bg-base-100">
+			<div class="flex-1">
+				<a class="btn btn-ghost text-lg">sasanqua.work</a>
+			</div>
+			<div class="flex-none">
+				<ul class="menu menu-horizontal px-1">
+					<li><a>Projects</a></li>
+					<li><a>Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		<slot />
+	</div>
+</template>
+<script setup lang="ts"></script>
