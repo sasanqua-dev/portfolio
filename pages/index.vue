5e7af63c6bee1a7f91e4291f0821ac26e2ed6ba2
Handa Kengo <78304794+sasanqua-dev@users.noreply.github.com>
2024-12-10 14:04:46 +0900

    fix: replace style code

--- pages/index.vue
+++ pages/index.vue
@@ -19,33 +19,4 @@
 		</div>
 	</div>
 </template>
-<style>
-	.card {
-		--ease-out-cubic: cubic-bezier(0.33, 1, 0.68, 1);
-		.face {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			backface-visibility: hidden;
-			transition: rotate 0.8s var(--ease-out-cubic);
-		}
-		.front {
-			rotate: 0 1 0 0deg;
-		}
-		.back {
-			rotate: 0 1 0 180deg;
-		}
-		@media (any-hover: hover) {
-			&:hover {
-				.front {
-					rotate: 0 1 0 180deg;
-				}
-				.back {
-					rotate: 0 1 0 360deg;
-				}
-			}
-		}
-	}
-</style>
+
