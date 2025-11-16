a7596d4f33009427609cbdb899b01107d1558f98
sasanqua <sasanqua.dev@gmail.com>
2025-11-16 17:40:04 +0900

    feat: Update event component to use new event schema and modify project icon URL

---
+++ schema/events.ts
@@ -0,0 +1,15 @@
+export interface Event {
+  id: string;
+  name: string;
+  category: string;
+  event: string;
+  repo: string;
+  reward: string;
+  slide: string;
+  sponsored: string;
+  start_date: string;
+  end_date: string;
+  created: string;
+  updated: string;
+  tag: string[];
+}
